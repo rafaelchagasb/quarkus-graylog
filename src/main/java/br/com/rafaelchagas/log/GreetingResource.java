2fff15fb3bb001b652cdd02e09d1c04e09bf7566
Rafael Chagas <rafa15chagas@gmail.com>
2022-10-04 15:20:06 -0300

    declare endpoint and properties graylog

--- src/main/java/br/com/rafaelchagas/log/GreetingResource.java
+++ src/main/java/br/com/rafaelchagas/log/GreetingResource.java
@@ -1,16 +1,24 @@
 package br.com.rafaelchagas.log;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger log = Logger.getLogger(GreetingResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello(@QueryParam("name") String name) {
+
+        log.info("Request received:" + name);
+
         return "Hello RESTEasy";
     }
 }
